04eb068a11936575e4ce6f8cb0fe39d97d4ebafa
PawelWil <119164049+PawelWil@users.noreply.github.com>
2023-01-05 08:53:37 +0100

    test update

--- info o użytkwoniku_lab5.py
+++ info o użytkwoniku_lab5.py
@@ -3,7 +3,7 @@ wiek = 35
 city = "Krakowie"
 
 # 1 sposób
-print(name, str(wiek), city)
+print(name, "lat:", str(wiek), "mieszka w:",  city)
 
 # 2 sposób, z użyciem concatenacji:
 print ("Mam na imię:" + str(name) + ", " + "mam:" + str(wiek) + " lat" + ", " + "mieszkam w:" + str(city) + "." )
