aaf3dca642964d5b6ec77e65dd519f5d9eab9687
PawelWil <119164049+PawelWil@users.noreply.github.com>
2023-01-09 20:37:17 +0100

    update

--- ZMIEnnE !.py
+++ ZMIEnnE !.py
@@ -1,43 +1,46 @@
 # !!! ZMIEnnE !!!
-# ważna uwaga - zmienne już w kodzie zawsze wpisuje bez apostrofów, czy cudzysłowów!!
+# ważna uwaga: zmienne w kodzie zawsze wpisuje bez apostrofów, czy cudzysłowów + z małych liter +
+# łącze je za pomocą podkreślników + nie mogą być za długie !!
 
-# 1. nAZWY ZMIEnnYCH
+# 1. NAZWY ZMIENNYCH
 # zmienne piszemy zawsze malymi literami + pomiędzy dwoma słowami zmiennej zawsze dajemy '_' +
 
 #var = 1 # to jest zmienna o nazwie var, ktra ma wartość 1
 #print(var) # tu wyswietlamy warość zmiennej var
 
-#account_balance = 1000. # zmienna o nazwie accout_balance - zmienna typu float
-#user = "Jan Kowalski" # zmienna typu ciąg znaków
-#print(user, account_balance)
+# account_balance = 1000. # zmienna o nazwie accout_balance - zmienna typu float
+# user = "Jan Kowalski" # zmienna typu ciąg znaków=string
+# print(user,", ma na koncie:", account_balance)
 
+# RÓŻnE spososoby zapisu zmiennych:
 #my_var = 345 # zmienna o nazwie my_var
-#number_of_days = 300 # zmienna o nazwie number_of_names
+#number_of_days = 300 # zmienna o nazwie number_of_days: 1 sposób, za pomocą podkreślnikow
 #total_years = 4 # zmienna o nazwie total_years
 
-#numberOfDays = 300 # zmienna o nazwie numberOfDays --> można tez taki typ nazw stosowa ale raz
+#numberOfDays = 300 # zmienna o nazwie numberOfDays: 2 sposób, za pomocą dużych liter, ale raczej tego unikać
 #totalYears = 4 # zmienna o nazwie totalYears
 
 
-# 2. Dynamiczne typowanie -- jak się to stsosuje to poprzednia wartość po prostu jest zastąpiona tą ostatnią, czyli w poniższym przypadku ostatnią wartością, któa się wyswietli to kokarda
+# 2. Dynamiczne typowanie -- jak się to stosuje to poprzednia wartość po prostu jest zastąpiona tą ostatnią, czyli w poniższym przypadku ostatnią wartością, która się wyswietli to kokarda
 
-# jak widać dodaliśmy do zmiennej a liczbę(int) + słowo(string) i powinna się wyświtlić kokarda, bo nadpisała 4
-#a = 4
-#print(a)
-#a ="kokarda"
-#print(a)
+# jak widać dodaliśmy do zmiennej 'a' liczbę(int) + słowo(string) i powinna się wyświtlić kokarda, bo nadpisała 4
+# a = 4
+# print(a)
+# a ="kokarda"
+# print(a)
 
 # 3. Złożone operatory przypisania
 
 # a. operator przypisania dla '+' i '-'
-x = 1 # chce podnieś tej zmiennej wartpość o 1, czyli jest to inkrementacja, przeciwieństwiem jest dekrementacja, czyli zmniejsznie. Może to być o 1 lub inną wartość
+# x = 1 # chce podnieś tej zmiennej wartość o 1, czyli jest to inkrementacja, przeciwieństwiem jest dekrementacja, czyli zmniejsznie. Może to być o 1 lub inną wartość
 # x = x + 1
-x += 1
-#print(x)
+# x += 1
+# print(x)
 
-# b. operator przypisania dla potęgowania '**'
-e = 2
-#e = e ** x
-e **= x
-print(e)
+# b. operator złożonego przypisania dla potęgowania '**'
+# e = 2
+# x = 2
+# e = e ** x
+# e **= x
+# print(e)
 
--- ZMIEnnE !_pythagorean.py
+++ ZMIEnnE !_pythagorean.py
@@ -1,12 +1,13 @@
 # to jest twierdzenie Pitagorasa
 
-a = 4. # zmienne typu float
+a = 4. # 'a' i 'b' to zmienne typu float - i one będę określać u nas dwie przyprostokątne
 b = 3.
 
+# c **2 = a**2 + b **2 # ale musimy teraz się pozbyć potęgowania do kwadratu, z przeciwprostokatnej 'c'
 c = (a ** 2 + b ** 2) ** .5 # c=a2 + b2 do pierwiastka, czyli do potęgi 1/2, czyli w kodzie do potęgi 0.5=.5
 
 print("Długość przeciwprostokątnej trójkąta o przyprostokątnych", a, "oraz", b, "wynosi", c, ".")
 # ważna uwaga - zmienne już w kodzie zawsze wpisuje bez apostrofów, czy cudzysłowów!!
 
 #teraz z użyciem concatenacji
-print("Długość przeciwprostokątnej trójkąta o przyprostokątnych" + str(a) + "oraz" str (b) + "wynosi" + str (c), ".")
+print("Długość przeciwprostokątnej trójkąta o przyprostokątnych" + str(a) + "oraz" + str (b) + "wynosi" + str (c), ".")
