4071cfef7b2f38e9af1ef7d14524ca32bcf84efd
PawelWil <119164049+PawelWil@users.noreply.github.com>
2023-01-09 19:30:13 +0100

    update

--- skrypt zawierający tylko jedną instrukcję.py
+++ skrypt zawierający tylko jedną instrukcję.py
@@ -1,5 +1,5 @@
-#pierwszy sposób
+#pierwszy sposób - za pomocą instrukcji nowej linii: \n
 print("Te trzy linie tekstu\nzostały wyświetlone\nza pomocą jednej instrukcji Pythona")
 
-#można to też tak zrobić
+#można to też tak zrobić - za pomocą wstawienia separatora nowej linii: sep = "\n"
 print("\nTe trzy linie tekstu", "zostały wyświetlone za pomocą", "jednej instrukcji Pythona.", sep="\n")
