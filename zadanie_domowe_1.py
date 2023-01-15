0efeeffda81f6ae4c292385e7578f80afea516c0
PawelWil <119164049+PawelWil@users.noreply.github.com>
2023-01-15 15:09:46 +0100

    update

--- zadanie_domowe_1.py
+++ zadanie_domowe_1.py
@@ -10,7 +10,7 @@ print("      **********")
 print("      ****  ****")
 print("      ***    ***")
 print("      **      **")
-print("      *        *")
+print("      *        *\n\n")
 
 
 # ZADANIE 2
@@ -22,8 +22,9 @@ print ("ZADANIE 2")
 _terabajt = 1024*1024
 a = _terabajt * 13
 b = 194
-
 print("Na pobranie danych z sieci o rozmiarze 13TB, potrzeba godzin:", round(((((a / b) * 5) / 3600)), 0))
+print("\n")
+
 
 # ZADANIE 3
  # Napisz skrypt pozwalający zasymulować zyski z lokat bankowych przy
@@ -35,27 +36,73 @@ print("Na pobranie danych z sieci o rozmiarze 13TB, potrzeba godzin:", round((((
  # • pokazać salda po każdym kwartale,
  # • wyliczyć roczny zysk.
 
-# inwestowane_srodki = 30_000. # własne inwestowane środki
-# deposit = inwestowane_srodki
-# oprocentowanie = 1.075
-#
-# deposit = deposit * oprocentowanie
-# print("Zysk z 3-miesięcznej lokaty bankowej, po I Kwartale, wynosi:", (deposit-inwestowane_srodki)/12 * 3, "zl.\n")
-#
-# deposit = deposit * oprocentowanie
-# print("Zysk z 3-miesięcznej lokaty bankowej, po II kwartale, wynosi:", (deposit - inwestowane_srodki) /12 * 3, "zl.\n")
-#
-# deposit = ((deposit * oprocentowanie))
-# print("Zysk z 3-miesięcznej lokaty bankowej, po III kwartale, wynosi:", (((deposit - 30_000) /12)*3), "zl.\n")
-#
-# deposit = ((deposit * oprocentowanie))
-# print("Zysk z 3-miesięcznej lokaty bankowej, po IV kwartale wynosi:", (((deposit - 30_000) /12)*3), "zl.\n")
-#
-# print ('Roczny zysk wynosi:', (((deposit - 30_000) /12)*3), "zl.\n")
-#
-#
+
+  #--------- A. KALKULACJA DLA OPROCENTOWANIA ROCZNEGO: 7,5%
+
+print("A. KALKULACJA DLA OPROCENTOWANIA ROCZNEGO: 7,5%")
+inwestowane_srodki = 30_000. # własne inwestowane środki
+deposit = inwestowane_srodki
+oprocentowanie = 0.075 #oprocentowanie w skali roku
+oprocentowanie_kwartalne = oprocentowanie / 4
+
+deposit = deposit * (1 + oprocentowanie_kwartalne)
+print("Zysk z 1-rocznej lokaty bankowej, po I Kwartale, wynosi:", (deposit-inwestowane_srodki), "zl.")
+
+deposit = deposit * (1 + oprocentowanie_kwartalne)
+print("Zysk z 1-rocznej lokaty bankowej, po II kwartale, wynosi:", (deposit - inwestowane_srodki), "zl.")
+
+deposit = deposit * (1 + oprocentowanie_kwartalne)
+print("Zysk z 1-rocznej lokaty bankowej, po III kwartale, wynosi:", (deposit - inwestowane_srodki), "zl.")
+
+deposit = deposit * (1 + oprocentowanie_kwartalne)
+print("Zysk z 1-rocznej lokaty bankowej, po IV kwartale wynosi:", (deposit - inwestowane_srodki), "zl.")
+
+print ('Roczny zysk wynosi:', (deposit - inwestowane_srodki), "zl.\n")
+
+
+#--------- B. KALKULACJA DLA OPROCENTOWANIA ROCZNEGO: 8,0%
+
+print("B. KALKULACJA DLA OPROCENTOWANIA ROCZNEGO: 8,0%")
+inwestowane_srodki = 30_000. # własne inwestowane środki
+deposit = inwestowane_srodki
+oprocentowanie = 0.080 #oprocentowanie w skali roku
+oprocentowanie_kwartalne = oprocentowanie / 4
+
+deposit = deposit * (1 + oprocentowanie_kwartalne)
+print("Zysk z 1-rocznej lokaty bankowej, po I Kwartale, wynosi:", (deposit-inwestowane_srodki), "zl.")
+
+deposit = deposit * (1 + oprocentowanie_kwartalne)
+print("Zysk z 1-rocznej lokaty bankowej, po II kwartale, wynosi:", (deposit - inwestowane_srodki), "zl.")
+
+deposit = deposit * (1 + oprocentowanie_kwartalne)
+print("Zysk z 1-rocznej lokaty bankowej, po III kwartale, wynosi:", (deposit - inwestowane_srodki), "zl.")
+
+deposit = deposit * (1 + oprocentowanie_kwartalne)
+print("Zysk z 1-rocznej lokaty bankowej, po IV kwartale wynosi:", (deposit - inwestowane_srodki), "zl.")
+
+print ('Roczny zysk wynosi:', (deposit - inwestowane_srodki), "zl.\n")
 
 
 
+#--------- C. KALKULACJA DLA OPROCENTOWANIA ROCZNEGO: 8,25%
 
+print("C. KALKULACJA DLA OPROCENTOWANIA ROCZNEGO: 8,25%")
+inwestowane_srodki = 30_000. # własne inwestowane środki
+deposit = inwestowane_srodki
+oprocentowanie = 0.0825 #oprocentowanie w skali roku
+oprocentowanie_kwartalne = oprocentowanie / 4
+
+deposit = deposit * (1 + oprocentowanie_kwartalne)
+print("Zysk z 1-rocznej lokaty bankowej, po I Kwartale, wynosi:", (deposit-inwestowane_srodki), "zl.")
+
+deposit = deposit * (1 + oprocentowanie_kwartalne)
+print("Zysk z 1-rocznej lokaty bankowej, po II kwartale, wynosi:", (deposit - inwestowane_srodki), "zl.")
+
+deposit = deposit * (1 + oprocentowanie_kwartalne)
+print("Zysk z 1-rocznej lokaty bankowej, po III kwartale, wynosi:", (deposit - inwestowane_srodki), "zl.")
+
+deposit = deposit * (1 + oprocentowanie_kwartalne)
+print("Zysk z 1-rocznej lokaty bankowej, po IV kwartale wynosi:", (deposit - inwestowane_srodki), "zl.")
+
+print ('Roczny zysk wynosi:', (deposit - inwestowane_srodki), "zl.\n")
 
