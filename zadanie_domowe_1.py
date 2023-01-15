1855079a41e5e4b8259c65f7ace3de34bea53d17
PawelWil <119164049+PawelWil@users.noreply.github.com>
2023-01-15 14:52:38 +0100

    update

--- zadanie_domowe_1.py
+++ zadanie_domowe_1.py
@@ -1,28 +1,29 @@
 # ZADANIE 1
  # Wyświetl na ekranie następujący wzór:
 
-# print("      *        *")
-# print("      **      **")
-# print("      ***    ***")
-# print("      ****  ****")
-# print("      **********")
-# print("      ****  ****")
-# print("      ***    ***")
-# print("      **      **")
-# print("      *        *")
-# test
+print ("ZADANIE 1 \n")
+print("      *        *")
+print("      **      **")
+print("      ***    ***")
+print("      ****  ****")
+print("      **********")
+print("      ****  ****")
+print("      ***    ***")
+print("      **      **")
+print("      *        *")
+
 
 # ZADANIE 2
  # Napisz skrypt pomagający oszacować ile godzin potrzeba na pobranie
  # danych z sieci o rozmiarze 13 TB, jeżeli wiesz, że plik o rozmiarze 194 MB
  # udało się pobrać w 5 sekund. Wynik zaokrąglij do pełnych godzin.
 
-# * 1 terabajt[TB] = 1_000_000 megabajtów[MB]
-# a = 13_000_000
-# b = 194
-# print (round((a / b) * 5) / 3600)
-# print(round(((((a / b) * 5) / 3600)), 0))
+print ("ZADANIE 2")
+_terabajt = 1024*1024
+a = _terabajt * 13
+b = 194
 
+print("Na pobranie danych z sieci o rozmiarze 13TB, potrzeba godzin:", round(((((a / b) * 5) / 3600)), 0))
 
 # ZADANIE 3
  # Napisz skrypt pozwalający zasymulować zyski z lokat bankowych przy
@@ -34,25 +35,25 @@
  # • pokazać salda po każdym kwartale,
  # • wyliczyć roczny zysk.
 
-inwestowane_srodki = 30_000. # własne inwestowane środki
-deposit = inwestowane_srodki
-oprocentowanie = 1.075
-
-deposit = deposit * oprocentowanie
-print("Zysk z 3-miesięcznej lokaty bankowej, po I Kwartale, wynosi:", (deposit-inwestowane_srodki)/12 * 3, "zl.\n")
-
-deposit = deposit * oprocentowanie
-print("Zysk z 3-miesięcznej lokaty bankowej, po II kwartale, wynosi:", (deposit - inwestowane_srodki) /12 * 3, "zl.\n")
-
-deposit = ((deposit * oprocentowanie))
-print("Zysk z 3-miesięcznej lokaty bankowej, po III kwartale, wynosi:", (((deposit - 30_000) /12)*3), "zl.\n")
-
-deposit = ((deposit * oprocentowanie))
-print("Zysk z 3-miesięcznej lokaty bankowej, po IV kwartale wynosi:", (((deposit - 30_000) /12)*3), "zl.\n")
-
-print ('Roczny zysk wynosi:', (((deposit - 30_000) /12)*3), "zl.\n")
-
-
+# inwestowane_srodki = 30_000. # własne inwestowane środki
+# deposit = inwestowane_srodki
+# oprocentowanie = 1.075
+#
+# deposit = deposit * oprocentowanie
+# print("Zysk z 3-miesięcznej lokaty bankowej, po I Kwartale, wynosi:", (deposit-inwestowane_srodki)/12 * 3, "zl.\n")
+#
+# deposit = deposit * oprocentowanie
+# print("Zysk z 3-miesięcznej lokaty bankowej, po II kwartale, wynosi:", (deposit - inwestowane_srodki) /12 * 3, "zl.\n")
+#
+# deposit = ((deposit * oprocentowanie))
+# print("Zysk z 3-miesięcznej lokaty bankowej, po III kwartale, wynosi:", (((deposit - 30_000) /12)*3), "zl.\n")
+#
+# deposit = ((deposit * oprocentowanie))
+# print("Zysk z 3-miesięcznej lokaty bankowej, po IV kwartale wynosi:", (((deposit - 30_000) /12)*3), "zl.\n")
+#
+# print ('Roczny zysk wynosi:', (((deposit - 30_000) /12)*3), "zl.\n")
+#
+#
 
 
 
